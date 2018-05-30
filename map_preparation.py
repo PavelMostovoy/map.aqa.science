ebf9a9619eec735d81bace2dcf0934c88712bb91
pavel.mostovoy <pavelmostocoy@gmail.com>
2018-05-30 08:25:46 +0300

    Case lowering

--- map_preparation.py
+++ map_preparation.py
@@ -4,4 +4,4 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
 map.add_child(folium.LayerControl())
 
-map.save("Templates//Map1.html")
+map.save("templates//Map1.html")
