ca9811c6905c7d6d6cfd2600cdd0621129d32119
pavel_mostovoy <pavelmostovoy@gmail.com>
2018-12-05 12:09:56 +0200

    minimization and remove navigation menu it should be added ASAP

--- init_map/admin.py
+++ init_map/admin.py
@@ -1,5 +1,6 @@
 from leaflet.admin import LeafletGeoAdmin
 from django.contrib import admin
-from .models import Spot
+from .models import Spot,Polygon
 
-admin.site.register(Spot, LeafletGeoAdmin)
+admin.site.register((Polygon,Spot),LeafletGeoAdmin)
+# admin.site.register(Polygon,LeafletGeoAdmin)
