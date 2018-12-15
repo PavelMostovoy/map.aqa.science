f5f436ad3f2b309d7c14e88baf0408ef5cd9c710
pavel.mostovoy <pavelmostocoy@gmail.com>
2018-12-15 10:28:38 +0200

    add Polygon

--- init_map/models.py
+++ init_map/models.py
@@ -1,19 +1,30 @@
-from djgeojson.fields import PointField
+from djgeojson.fields import PointField, PolygonField
 from django.db import models
 import datetime
 
+
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/date/instance/<filename>
-    return '{0}/spot_id_{1}/{2}'.format(datetime.datetime.utcnow().date(),instance.id, filename)
+    return '{0}/spot_id_{1}/{2}'.format(datetime.datetime.utcnow().date(), instance.id, filename)
+
 
 class Spot(models.Model):
-
     geom = PointField()
     description = models.TextField(default="mock description")
-    picture = models.ImageField(default="mock image",upload_to=user_directory_path)
+    picture = models.ImageField(default="mock image", upload_to=user_directory_path)
 
     @property
     def popup_content(self):
         return '<img src="{}" /><p>{}</p>'.format(
             self.picture.url,
+            self.description)
+
+
+class Polygon(models.Model):
+    geom = PolygonField(models.Model)
+    description = models.TextField(default="mock description")
+
+    @property
+    def popup_content(self):
+        return '<p>{}</p>'.format(
             self.description)
